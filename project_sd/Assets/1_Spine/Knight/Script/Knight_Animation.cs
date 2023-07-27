6e85fd6dcc02f83922ebb8ed3fc1541dec99a320
이 재욱 <twa072217@naver.com>
2023-07-27 17:55:34 +0900

    프로젝트 생성, 스파인 애니메이션 작동

---
+++ project_sd/Assets/1_Spine/Knight/Script/Knight_Animation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class Knight_Animation : MonoBehaviour
+{
+    [SpineAnimation]
+    public string idleAnimationName;
+
+    SkeletonAnimation skeletonAnimation;
+
+    public Spine.AnimationState spineAnimationState;
+    public Spine.Skeleton skeleton;
+
+    public float idleDuration = 1.0f;
+
+    void Start()
+    {
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+        spineAnimationState = skeletonAnimation.AnimationState;
+        skeleton = skeletonAnimation.Skeleton;
+
+        StartCoroutine(DoDemoRoutine());
+    }
+
+    IEnumerator DoDemoRoutine()
+    {
+        spineAnimationState.SetAnimation(0, idleAnimationName, true);
+        yield return new WaitForSeconds(idleDuration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
